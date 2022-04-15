327149940c91b9db62cf6fec7173909a096037db
digitradiart <digitradiart@gmail.com>
2022-04-15 22:36:41 +0700

    service worker

--- js/sw.js
+++ js/sw.js
@@ -10,3 +10,42 @@ self.addEventListener('install', function (event) {
     })
   );
 });
+
+// FETCH AND ACTIVATE CACHE
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.fetch).then(function (response) {
+        // check if we receive valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+        }
+
+        var responseToCatch = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCatch);
+        });
+        return response;
+      });
+    })
+  );
+});
+
+// ACTIVATE
+self.addEventListener('activate', function (event) {
+  var cacheWaitList = CACHE_NAME;
+  event.waitUntil(
+    caches.keys.then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWaitList.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
